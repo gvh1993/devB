ba417c83f8ff4870d3ae3638c82faedf5a2bcf7a
gvh1993 <gertjanvanh@live.nl>
2016-04-19 13:48:08 +0200

    initial commit

---
+++ WebApplication-dataflow/UnitTestProject1/Controllers/WebApi/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication_dataflow.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication_dataflow.Controllers.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void IsConnectedTest()
+        {
+            WebApiController api = new WebApiController();
+
+            bool result = api.IsConnected(true);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ WebApplication-dataflow/WebApplication-dataflow/App_Start/BundleConfig.cs
@@ -0,0 +1,61 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication_dataflow {
+    public class BundleConfig {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new ScriptBundle("~/bundles/pdf").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        //"~/Scripts/rasterizeHTML/*.js",
+                        "~/Scripts/jspdf.js",
+                        "~/Scripts/plugins/*.js",
+                        "~/Scripts/html2canvas.min.js",
+                        "~/Scripts/pdf.generator.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/buydata").Include(
+                        "~/Scripts/buydata.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new StyleBundle("~/Content/DashboardStyle").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/Dashboard.css"));
+            bundles.Add(new StyleBundle("~/Content/HomeStyle").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/Home.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dashboardController").Include(
+                        "~/Scripts/AngularControllers/DashboardController.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/DiskSpace").Include(
+                    "~/Scripts/AngularControllers/DiskSpaceAnalysis/DiskSpaceController.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/WebApi").Include(
+                    "~/Scripts/AngularControllers/DiskSpace/WebApiFactory.js"));
+
+            
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
