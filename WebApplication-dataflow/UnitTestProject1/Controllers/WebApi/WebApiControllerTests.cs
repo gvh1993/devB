4b461100e5d7ef9bdd24c07f8a5122ac4b4c7216
gvh1993 <gertjanvanh@live.nl>
2016-04-20 13:46:35 +0200

    commit test

---
+++ WebApplication-dataflow/UnitTestProject1/Controllers/WebApi/WebApiControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication_dataflow.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace WebApplication_dataflow.Controllers.Tests
+{
+    [TestClass()]
+    public class WebApiControllerTests
+    {
+        [TestMethod()]
+        public void IsConnectedTest()
+        {
+            WebApiController api = new WebApiController();
+
+            bool result = api.IsConnected(true);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
